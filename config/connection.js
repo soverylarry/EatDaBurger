de69b8907f5f4313d3ac38e4c57e51bd23892015
larry laconi <soverylarry@gmail.com>
2020-09-12 16:14:09 -0400

    updating Heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql");
 
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Marathon705!",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Marathon705!",
+        database: "burgers_db"
+    });
+}
+
+connection.connect();
 
 connection.connect(function(err) {
     if (err) {
